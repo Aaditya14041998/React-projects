039467bdca28696cd9af74a66d071554a029a686
aaditya shukla <131380763+Aaditya14041998@users.noreply.github.com>
2025-02-22 23:20:53 +0530

    changes related to css

---
+++ 02counter/src/image.jsx
@@ -0,0 +1,9 @@
+import backgroundImage from './133677653507313007.jpg';
+// Import other images as needed
+
+const Images = {
+  backgroundImage,
+  // Add other images here
+};
+
+export default Images;
